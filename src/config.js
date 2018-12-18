852a8b3291ca0b46a5dc930a62a1d45ae07147fd
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2018-12-17 21:41:59 -0500

    remove express dependency
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/config.js
+++ src/config.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (configFile) => {
-  let config = require(path.resolve(configFile));
+  let config = require(configFile);
+  const baseDir = path.dirname(configFile);
 
   // load https certs file content
   if (config && config.https) {
     ['key', 'cert', 'pfx'].forEach(key => {
       if (config.https[key]) {
-        let file = path.resolve(config.https[key]);
+        let file = path.resolve(baseDir, config.https[key]);
         config.https[key] = fs.readFileSync(file);
       }
     });
--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@
  */
 
 const path = require('path');
-const express = require('express');
+const fs = require('fs');
 const https = require('https');
 
 // load package meta
 const pkg = require('../package.json');
 const version = pkg && pkg.version;
 const name = pkg && pkg.name;
+const rootDir = path.resolve(__dirname, '..');
 
 // define args
 var argv = require('yargs')
@@ -26,30 +27,114 @@ var argv = require('yargs')
   .usage('Usage: $0 [options]')
   .option('C', {
     alias: 'config',
-    default: 'configs/config-default.json',
+    default: 'config-default.json',
     description: 'config JSON file',
+  })
+  .option('v', {
+    alias: 'verbose',
+    default: false,
+    description: 'show request logs',
+    type: 'boolean'
   })
   .help('h')
   .alias('h', 'help')
   .argv;
 
 // load config
-const config = require('./config')(argv.config);
+const configFile = path.resolve(rootDir, 'configs', argv.config);
+const config = require('./config')(configFile);
 const serviceFor = config['service-for'] || name;
 
-// define app
-const app = express();
+// mime
+const mimeList = {
+  js: 'text/javascript',
+  css: 'text/css',
+  json: 'application/json',
+  png: 'image/png',
+  jpg: 'image/jpg',
+  svg: 'image/svg+xml'
+};
+const mimeDefault = 'text/html';
 
 // server static folder
+process.stdout.write(`[${serviceFor}] config file ${configFile}:\n`);
 process.stdout.write(`[${serviceFor}] paths will be served:\n`);
+const paths = [];
 for (let one of config.paths) {
-  const baseDir = path.resolve(one.dir);
+  const baseDir = path.resolve(rootDir, one.dir);
   process.stdout.write(`[${serviceFor}]   - ${one.uri} => ${baseDir}\n`);
-  app.use(one.uri, express.static(baseDir));
+  paths.push({ uri: one.uri, dir: baseDir });
 }
 
+// define app
+const requestHandler = (request, response) => {
+  let url = request.url;
+  if (url.substr(-1) === '/') {
+    url = url + 'index.html';
+  }
+
+  // write log
+  const writeLog = (url, code, file) => {
+    if (!argv.verbose) {
+      return;
+    }
+
+    const ts = new Date();
+    const tss = ts.toUTCString();
+    process.stdout.write(`[${serviceFor}][${tss}] ${request.url} ${code} ${file}\n`);
+  };
+
+  // locate dir based on path
+  let baseuri = null;
+  let basedir = null;
+  let file = null;
+  for (let one of paths) {
+    if (url.substr(0, one.uri.length) === one.uri) {
+      baseuri = one.uri;
+      basedir = one.dir;
+      break;
+    }
+  };
+  if (baseuri) {
+    file = path.resolve(basedir, url.substr(baseuri.length + 1));
+  }
+
+  // decide content type
+  let ext = path.extname(url).toLowerCase();
+  let contentType = mimeDefault;
+  if (mimeList[`.${ext}`]) {
+    contentType = mimeList[`.${ext}`];
+  }
+
+  if (file) {
+    // read file
+    fs.readFile(file, (error, content) => {
+      if (error) {
+        if (error.code == 'ENOENT') {
+          writeLog(request.url, 404, file);
+          response.writeHead(404, { 'Content-Type': 'text/plain' });
+          response.end('File not found');
+        } else {
+          writeLog(request.url, 500, `error#${error.code}`);
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Read file failed with error: ' + error.code + ' ..\n');
+          response.end();
+        }
+      } else {
+        writeLog(request.url, 200, file);
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content);
+      }
+    });
+  } else {
+    writeLog(request.url, 404, `error#`);
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('File not found');
+  }
+};
+
 // start server
-https.createServer(config.https, app)
+https.createServer(config.https, requestHandler)
   .listen(config.port, '0.0.0.0', () => {
-    process.stdout.write(`[${serviceFor}] is started and listening on ${config.port}...\n`);
+    process.stdout.write(`[${serviceFor}] is started and listening on ${config.port}...\n\n`);
   });
